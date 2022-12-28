964c028b2705f82da5c700926edef849e959c7f4
Vic P <vic4key@gmail.com>
2022-12-29 01:58:07 +0700

    Initial

--- vn_ime.py
+++ vn_ime.py
@@ -7,33 +7,11 @@ from .bogo.core import get_vni_definition, process_sequence
 import sublime, sublime_plugin
 
 STATUS = False
-TELEX = False
+TELEX  = False
 
 class SaveOnModifiedListener(sublime_plugin.EventListener):
   def on_modified_async(self, view):
-    view.run_command('startime')
-
-class StartimeCommand(sublime_plugin.TextCommand):
-  def run(self, edit):
-    if not STATUS: return False
-    curr_pos = self.view.sel()[0]
-    word_region = self.view.word(curr_pos)
-    word = self.view.substr(word_region)
-    final = self.process(word)
-    #
-    if not final: return False
-    self.view.end_edit(edit)
-    self.view.run_command("runchange", {"string":final})
-    return True
-
-  def process(self, word):
-    if TELEX:
-      final_word = process_sequence(word)
-    else:
-      final_word = process_sequence(word, rules=get_vni_definition())
-    if final_word != word:
-      return final_word
-    return False
+    view.run_command("startime")
 
 class ControlimeCommand(sublime_plugin.TextCommand):
   def run(self, edit):
@@ -53,20 +31,36 @@ class ControlimeCommand(sublime_plugin.TextCommand):
       STATUS = True
       self.view.set_status('VN IME'," VN IME: ON")
 
-class RunchangeCommand(sublime_plugin.TextCommand):
-  def run(self, edit, string):
-    curr_pos = self.view.sel()[0]
-    region = self.view.word(curr_pos)
-    self.view.replace(edit, region, string)
+class StartimeCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    if not STATUS: return False
+    cur_pos = self.view.sel()[0]
+    cur_region = self.view.word(cur_pos)
+    cur_word = self.view.substr(cur_region)
+    new_word = self.process(cur_word)
+    if not new_word: return False
+    self.view.end_edit(edit)
+    self.view.run_command("bridge_replace_text", {"text": new_word})
+    print(cur_word, "=>", new_word)
+    return True
 
-class FuncundoCommand(sublime_plugin.WindowCommand):
-  def run(self):
-    self.window.run_command("undo")
+  def process(self, word):
+    if TELEX:
+      final_word = process_sequence(word)
+    else:
+      final_word = process_sequence(word, rules=get_vni_definition())
+    if final_word != word:
+      return final_word
+    return False
 
-class FuncpasteCommand(sublime_plugin.WindowCommand):
-  def run(self):
-    self.window.run_command("paste")
-
-class FuncredoCommand(sublime_plugin.WindowCommand):
-  def run(self):
-    self.window.run_command("redo")
+class BridgeReplaceTextCommand(sublime_plugin.TextCommand):
+  '''
+  SLT did not use the Edit object in the run(...) method
+  Thus, if we replace text inside this method is not working
+  So on, we write a bridge class to do text replacement during typing to bypass this issue
+  ValueError("Edit objects may not be used after the TextCommand's run method has returned")
+  '''
+  def run(self, edit, text):
+    cur_pos = self.view.sel()[0]
+    cur_region = self.view.word(cur_pos)
+    self.view.replace(edit, cur_region, text)
